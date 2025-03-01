b9cc97247f5837c713d0cc1d99a4615bd4d96e35
Abusolay <sefat01625@gmail.com>
2025-03-02 01:16:07 +0600

    card

--- js/script.js
+++ js/script.js
@@ -27,94 +27,48 @@ document.getElementById("themeButton").addEventListener("click", function () {
 //     }
 // });
 
-// document
-// .getElementById('add-money')
-// .addEventListener('click', function(event){
-//     event.preventDefault();
-//     const amount = getInputValueById('amount');
-//     const pin = getInputValueById('pin');
-//     const acount = document.getElementById('account-number').value;
-//     const mainBalance = getInnerTextById('main-balance')
-//     if(acount.length === 11){
-//         if(pin === 1234){
-//             const sum = mainBalance + amount;
-//             // document.getElementById('main-balance').innerText = sum;
-//             setInnerTextByIdAndValue('main-balance', sum);
+document
+.getElementById('add-money')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    const amount = getInputValueById('add-money');
+    const pin = getInputValueById('pin');
+    const acount = document.getElementById('account-number').value;
+    const mainBalance = getInnerTextById('main-balance')
+    if(acount.length === 11){
+        if(pin === 1234){
+            const sum = mainBalance + amount;
+            // document.getElementById('main-balance').innerText = sum;
+            setInnerTextByIdAndValue('main-balance', sum);
 
-//             const container = document.getElementById('transection-cont')
-//             const p = document.createElement('p');
-//             p.innerText =`
-//             added ${amount} from ${acount} account
-//             `
-//             container.appendChild(p)
-//         }
-//         else{
-//             console.log('pin thik nai')
-//         }
-//     }
-//     else{
-//         console.log('account numbet thik nai')
-//     }
-// })
+            const container = document.getElementById('transection-cont')
+            const p = document.createElement('p');
+            p.innerText =`
+            added ${amount} from ${acount} account
+            `
+            container.appendChild(p)
+        }
+        else{
+            console.log('pin thik nai')
+        }
+    }
+    else{
+        console.log('account numbet thik nai')
+    }
+})
 
-// const themeToggle = document.querySelector('.checkbox');  
-// themeToggle.addEventListener('change', () => {  
-//     document.documentElement.classList.toggle('dark');  
-//     localStorage.setItem('theme', document.documentElement.classList.contains('dark') ? 'dark' : 'light');  
-// });  
-// window.onload = () => {  
-//     if (localStorage.getItem('theme') === 'dark') {  
-//         document.documentElement.classList.add('dark');  
-//         themeToggle.checked = true;  
-//     }  
-// };
+
 
 
 // document.addEventListener("click", function (event) {
-//     const dropdown = document.querySelector(".dropdown");
-//     if (!dropdown.contains(event.target)) {
-//         dropdown.classList.remove("open");
-//     } else {
-//         dropdown.classList.toggle("open");
-//     }
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let btn = document.getElementById("myButton"); 
-//     if (btn) {
-//         btn.addEventListener("click", function () {
-//             alert("Button Clicked");
-//         });
-//     } else {
-//         console.log("Element not found!");
+//     if (event.target.id === "myButton") {
+//         alert("Button Clicked!");
 //     }
 // });
 
 
 
-// document.getElementById("addTaskBtn").addEventListener("click", function() {
-//     const taskBoard = document.querySelector(".grid");
-//     const newTask = document.createElement("div");
-
-//     newTask.className = "card card-border text-2xl text-black";
-//     newTask.innerHTML = `
-//         <div class="card-body">
-//             <h2 class="card-title">New Project</h2>
-//             <h1>New Task</h1>
-//             <p>New task description goes here.</p>
-//             <div class="card-actions">
-//                 <h2>Deadline</h2>
-//                 <p>30 March 2025</p>
-//                 <button class="btn btn-primary">Completed</button>
-//             </div>
-//         </div>
-//     `;
-
-//     taskBoard.appendChild(newTask);
-// });
-
-
-// ** 
+//** 
 document.addEventListener("DOMContentLoaded", function () {
     const dateElement = document.querySelector(".calendar-date");
     if (dateElement) {
@@ -124,19 +78,4 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-// document.querySelector("").addEventListener("click", function () {
-    
-// });
-
-document.getElementById("addTaskBtn").addEventListener("click", function() {
-    const taskBoard = document.querySelector(".flex");  
-    const newTask = document.createElement("div");
-
-    newTask.className = "p-3 bg-green-400 text-black";
-    newTask.innerHTML = `
-        <h2 class="text-xl font-semibold">Fix Mobile Button Issue</h2>
-    `;
-
-    taskBoard.appendChild(newTask);
-});
 
